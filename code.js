bbf189e849f66ae752e8d7dffd20a69546837a18
Strombreaker07 <80044297+Strombreaker07@users.noreply.github.com>
2022-12-28 13:11:47 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,12 @@
+function check()
+{
+    var answer = document.getElementById("ans_checker").value;
+    if (answer == 55)
+    {
+        window.location = "hash.html"
+    }
+    else
+    {
+        window.alert("wrong code")
+    }
+}
